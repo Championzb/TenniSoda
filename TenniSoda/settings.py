de2f96caec554e47617e6aadcc251cb1fd712fd1
damon <zhangbin.1101@gmail.com>
2014-05-31 14:13:15 -0400

    add app in setting.py

--- TenniSoda/settings.py
+++ TenniSoda/settings.py
@@ -36,6 +36,10 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'south',
+    'account',
+    'userprofile',
+    'notification',
 )
 
 MIDDLEWARE_CLASSES = (
