92586074237a46954bb25d06b8aa86d887dab0f1
damon <zhangbin.1101@gmail.com>
2014-06-02 18:35:20 -0400

    User Login

--- account/views.py
+++ account/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib import auth
 from django.core.context_processors import csrf
+from django.contrib.auth.decorators import login_required
 
 from admin import UserCreationForm
 
@@ -34,3 +35,32 @@ def register_user(request):
 
 def register_success(ruquest):
     return render_to_response('register_success.html')
+
+def login(request):
+    args = {}
+    args.update(csrf(request))
+    return render_to_response('login.html', args)
+
+def auth_view(request):
+    email = request.POST.get('email', '')
+    password = request.POST.get('password', '')
+    user = auth.authenticate(email=email, password=password)
+
+    if user is not None:
+        auth.login(request, user)
+        return HttpResponseRedirect('/account/welcome_user')
+    else:
+        return HttpResponseRedirect('/account/invalid_login')
+
+@login_required
+def welcome_user(request):
+    username = request.user.email.split('@')[0]
+    return render_to_response('welcome_user.html',
+        {'username':username,})
+
+def invalid_login(request):
+    return render_to_response('invalid_login.html')
+
+def logout(request):
+    auth.logout(request)
+    return render_to_response('login.html')
