d400269b95758466389c7a9d50a4d0025462aaa0
Haochen Wu <wuhaochen1990@gmail.com>
2014-06-06 11:02:48 -0400

    add Leagueadmin action for arrange game

--- game/admin.py
+++ game/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
 from models import League, Game, Score, GroupStage
-# Register your models here.
 
+def game_arrange(modeladmin,request,queryset):
+	for league in queryset:
+		#arrange game for a league
+		print league.id
 
-admin.site.register(League)
+class LeagueAdmin(admin.ModelAdmin): 
+	list_display = ['name','city','start_date','end_date']
+	ordering = ['start_date']
+	actions = [game_arrange]
+		
+
+admin.site.register(League,LeagueAdmin)
 admin.site.register(Game)
 admin.site.register(Score)
 admin.site.register(GroupStage)
