f482bff2d4b01f037f4f016342c82b534fbc82f3
Haochen Wu <wuhaochen1990@gmail.com>
2014-06-06 11:40:51 -0400

    complete game arrange

--- game/admin.py
+++ game/admin.py
@@ -4,8 +4,17 @@ from models import League, Game, Score, GroupStage
 def game_arrange(modeladmin,request,queryset):
 	for league in queryset:
 		#arrange game for a league
+		group_num = 1
+		member_num = 1
 		for player in league.players.all():
-			GroupStage.objects.create(league=league,group_number=1,member_number=1,player=player)
+			if member_num < 5:
+				GroupStage.objects.create(league=league,group_number=group_num,member_number=member_num,player=player)
+				member_num += 1
+			else:
+				group_num += 1
+				member_num = 1
+				GroupStage.objects.create(league=league,group_number=group_num,member_number=member_num,player=player)
+				
 		
 class LeagueAdmin(admin.ModelAdmin): 
 	list_display = ['name','city','start_date','end_date']
