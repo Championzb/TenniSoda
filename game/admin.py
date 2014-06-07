dcf8bbff5478f7cb466d2b552f74109977954bc9
damon <zhangbin.1101@gmail.com>
2014-06-07 12:52:56 -0400

    fix adding arrange_game

--- game/admin.py
+++ game/admin.py
@@ -4,17 +4,15 @@ from models import League, Game, Score, GroupStage
 def game_arrange(modeladmin,request,queryset):
 	for league in queryset:
 		#arrange game for a league
-		group_num = 1
-		member_num = 1
+		group_num = 4
+
+		group_index = 1
+		member_index = 0
 		for player in league.players.all():
-			if member_num < 5:
-				GroupStage.objects.create(league=league,group_number=group_num,member_number=member_num,player=player)
-				member_num += 1
-			else:
-				group_num += 1
-				member_num = 1
-				GroupStage.objects.create(league=league,group_number=group_num,member_number=member_num,player=player)
-				
+			GroupStage.objects.create(league=league,group_number=group_index,member_number=member_index/group_num+1,player=player)
+			group_index = (group_index % group_num) + 1
+			member_index += 1
+
 		
 class LeagueAdmin(admin.ModelAdmin): 
 	list_display = ['name','city','start_date','end_date']
@@ -23,9 +21,16 @@ class LeagueAdmin(admin.ModelAdmin):
 
 class GroupStageAdmin(admin.ModelAdmin):
 	list_display = ['league','group_number','member_number','player','points']
+
+class GameAdmin(admin.ModelAdmin):
+	list_display = ['league','court','player1','player2','winner','date']
+	ordering = ['date']
+
+class ScoreAdmin(admin.ModelAdmin):
+	list_display = ['game','score11','score12','score21','score22','score31','score32','score41','score42','score51','score52','is_confirmed']
 		
 
 admin.site.register(League,LeagueAdmin)
-admin.site.register(Game)
-admin.site.register(Score)
+admin.site.register(Game,GameAdmin)
+admin.site.register(Score,ScoreAdmin)
 admin.site.register(GroupStage,GroupStageAdmin)
