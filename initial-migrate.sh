1c3cbd873f4a0e643399df6a54662cdc2fd77dbe
Bin Zhang <zhangbin.1101@gmail.com>
2014-07-26 18:42:07 -0400

    .sh

---
+++ initial-migrate.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+./manage.py syncdb
+./manage.py schemamigration account --initial
+./manage.py schemamigration city --initial
+./manage.py schemamigration court --initial
+./manage.py schemamigration game --initial
+./manage.py schemamigration notification --initial
+./manage.py schemamigration review --initial
+./manage.py migrate friendship
+./manage.py migrate account --fake
+./manage.py migrate city --fake
+./manage.py migrate court --fake
+./manage.py migrate game --fake
+./manage.py migrate notification --fake
+./manage.py migrate review --fake
