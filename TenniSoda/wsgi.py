d79af9eb76befa7314aa8d417916018da65edce9
Bin Zhang <zhangbin.1101@gmail.com>
2014-08-22 15:11:12 -0400

    WSGI

--- TenniSoda/wsgi.py
+++ TenniSoda/wsgi.py
@@ -6,9 +6,21 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
-
+"""
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TenniSoda.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+"""
+import os
+import sys
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'TenniSoda.settings'
+
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
+
+path = '/root/TenniSoda'
+if path not in sys.path:
+	sys.path.append(path)
